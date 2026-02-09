57c4ac0c814d5bac1beb46571467044b66bd2e3e
Nitesh Jaiswal <nitesh.singh1423.nl@gmail.com>
2026-02-09 18:29:01 +0545

    Added View Models for Create Lobby and TeamSetup.

---
+++ AuctionArena/Models/CreateLobbyViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AuctionArena.Models
+{
+    public class CreateLobbyViewModel
+    {
+        public string HostName { get; set; } = string.Empty;
+        public string GameName { get; set; } = string.Empty;
+        public string? Password { get; set; }
+        public int TotalTeams { get; set; }
+        public int PlayersPerTeam { get; set; }
+        public int PointsPerTeam { get; set; }
+        public int MinPlayersPerTeam { get; set; }
+        public int MaxPlayersPerTeam { get; set; }
+        public List<TeamSetup> Teams { get; set; } = new();
+    }
+}
---
+++ AuctionArena/Models/TeamSetup.cs
@@ -0,0 +1,9 @@
+﻿namespace AuctionArena.Models
+{
+    public class TeamSetup
+    {
+        public string TeamName { get; set; } = string.Empty;
+        public string OwnerName { get; set; } = string.Empty;
+        public string? CaptainName { get; set; }
+    }
+}
