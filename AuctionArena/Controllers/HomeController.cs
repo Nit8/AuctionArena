8ed819938dfd3a26af5c72ca3d7550f5c7a1c50b
Nitesh Jaiswal <nitesh.singh1423.nl@gmail.com>
2026-02-15 19:23:05 +0545

    Add About page and update project dependencies
    Added an About action and view to display app info. Updated AuctionArena.csproj to include code generation tools.

--- AuctionArena/Controllers/HomeController.cs
+++ AuctionArena/Controllers/HomeController.cs
@@ -16,6 +16,12 @@ namespace AuctionArena.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewBag.Message = "Welcome to Auction Arena! This is a platform where you can create and join lobbies to participate in exciting player auctions. Whether you're a team owner looking to build your dream team or a player eager to showcase your skills, Auction Arena has something for everyone. Join us and experience the thrill of the auction floor!";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
