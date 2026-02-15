d715accb23a1383274b3afd17ac8a95f5772da11
Nitesh Jaiswal <nitesh.singh1423.nl@gmail.com>
2026-02-15 20:42:11 +0545

    Updated ActionHub with necessary jobs

--- AuctionArena/Hubs/ActionHub.cs
+++ AuctionArena/Hubs/ActionHub.cs
@@ -1,19 +1,47 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 
-namespace AuctionApp.Hubs
+namespace AuctionArena.Hubs
 {
-    public class AuctionHub : Hub
+    public class ActionHub : Hub
     {
-        // Client joins a lobby group
         public async Task JoinLobby(string lobbyId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, lobbyId);
         }
 
-        // Send bid update to everyone in lobby
+        public async Task LeaveLobby(string lobbyId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, lobbyId);
+        }
+
         public async Task SendBidUpdate(string lobbyId, object bidData)
         {
             await Clients.Group(lobbyId).SendAsync("ReceiveBidUpdate", bidData);
+        }
+
+        public async Task SendPlayerUpdate(string lobbyId, object playerData)
+        {
+            await Clients.Group(lobbyId).SendAsync("ReceivePlayerUpdate", playerData);
+        }
+
+        public async Task SendAuctionStateUpdate(string lobbyId, object stateData)
+        {
+            await Clients.Group(lobbyId).SendAsync("ReceiveAuctionStateUpdate", stateData);
+        }
+
+        public async Task SendTeamUpdate(string lobbyId, object teamData)
+        {
+            await Clients.Group(lobbyId).SendAsync("ReceiveTeamUpdate", teamData);
+        }
+
+        public async Task SendPauseUpdate(string lobbyId, bool isPaused)
+        {
+            await Clients.Group(lobbyId).SendAsync("ReceivePauseUpdate", isPaused);
+        }
+
+        public async Task SendPlayerSold(string lobbyId, object soldData)
+        {
+            await Clients.Group(lobbyId).SendAsync("ReceivePlayerSold", soldData);
         }
     }
 }
